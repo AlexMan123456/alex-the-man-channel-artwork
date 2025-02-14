6e5884b21e559f1c47bca9edd1f8ec5e6d121230
AlexMan123456 <alex.man03888@gmail.com>
2025-02-14 00:31:01 +0000

    Created profile picture

--- profile-picture.py
+++ profile-picture.py
@@ -3,7 +3,17 @@ from manim import *
 # manim profile-picture.py -qm ProfilePicture
 class ProfilePicture(Scene):
     def construct(self):
-        circle = Circle(radius=4, color="#FF1A1A").set_opacity(1)
-        capital_a = Text("A", color="#240CFF", font="Futura").scale(3)
+        circle_with_a = VGroup(Circle(radius=2, color="#FF1A1A").set_opacity(1), Text("A", color="#240CFF", font="Futura").scale(3.5)).move_to([-1,1.5,0]).scale(0.5)
 
-        self.add(circle, capital_a)
+        lex = Text("lex", color="#240CFF", font="Futura").scale(2).next_to(circle_with_a, RIGHT, buff=0.15)
+
+        the = Text("the", color=ORANGE, font="Futura").scale(1.5).next_to(VGroup(circle_with_a, lex), DOWN).shift(0.4*RIGHT+0.2*UP)
+
+        man = Text("MAN", color=YELLOW, font="Futura").scale(2).next_to(the, DOWN).shift(0.2*LEFT+0.1*DOWN)
+
+        self.add(
+            # NumberPlane(axis_config={"include_numbers":True}), 
+            circle_with_a, 
+            lex, 
+            the, 
+            man)
